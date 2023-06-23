fb636036aefc2431fef062a6789282a19dda7cdd
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2023-06-23 17:19:12 +0000

    Many slides on animated carousel

--- js/products-carousel-animated.js
+++ js/products-carousel-animated.js
@@ -4,18 +4,34 @@ const prevButton = carousel.querySelector('.carousel__button--prev');
 const nextButton = carousel.querySelector('.carousel__button--next');
 const productCards = carouselInner.querySelectorAll('.product-card');
 
+let slidesPerView = 1;
 let currentIndex = 0;
 
+function updateSlidesPerView() {
+    if (window.innerWidth >= 1024) {
+        slidesPerView = 3;
+    } else if (window.innerWidth >= 768) {
+        slidesPerView = 2;
+    } else {
+        slidesPerView = 1;
+    }
+    updateCarousel();
+}
+
 function updateCarousel() {
-    carouselInner.style.transform = `translateX(-${currentIndex * 100}%)`;
+    carouselInner.style.transform = `translateX(-${currentIndex * (100 / slidesPerView)}%)`;
 }
 
 prevButton.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + productCards.length) % productCards.length;
+    currentIndex = Math.max(currentIndex - 1, 0);
     updateCarousel();
 });
 
 nextButton.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % productCards.length;
+    currentIndex = Math.min(currentIndex + 1, productCards.length - slidesPerView);
     updateCarousel();
 });
+
+window.addEventListener('resize', updateSlidesPerView);
+
+updateSlidesPerView();
